e907943f463510f3e78b278d365a28ea54b32e28
Dmitry <kuzhelko.dmitry@yandex.ru>
2018-01-28 18:28:06 +0300

    make release APK

--- app/src/main/java/kuzhelko_dmitry/simplenotes/presentation/SplashActivity.java
+++ app/src/main/java/kuzhelko_dmitry/simplenotes/presentation/SplashActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import kuzhelko_dmitry.simplenotes.R;
 import kuzhelko_dmitry.simplenotes.presentation.notesList.view.NotesActivity;
 
 /**
@@ -16,7 +15,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_layout);
         Intent intent = new Intent(this, NotesActivity.class);
         startActivity(intent);
         finish();
